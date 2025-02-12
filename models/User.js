727a82c7c6b2c1bd683d9c188d2df5d8135ec31e
reynaldabnerr <reynald030685@gmail.com>
2025-02-12 14:36:06 +0800

    Menambahkan fitur register, login, dan upload bukti transfer

--- models/User.js
+++ models/User.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  nama: { type: String, default: "" },
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
+  nama: { type: String, default: "" },
   buktiTransfer: { type: String, default: "" }, // Untuk menyimpan gambar bukti transfer
   ticketId: { type: String, default: "" }, // Untuk menyimpan ID tiket setelah transfer berhasil
   qrCode: { type: String, default: "" }, // Untuk menyimpan QR Code
